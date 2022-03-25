c5879ad31a3f93dacd41e05601b9260896fa65db
rjdroege <99696219+rjdroege@users.noreply.github.com>
2022-03-24 20:04:25 -0500

    Exercise One

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChildOneComponent } from './child-one/child-one.component';
+import { ChildTwoComponent } from './child-one/child-two/child-two.component';
+import { ChildThreeComponent } from './child-one/child-two/child-three/child-three.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildOneComponent,
+    ChildTwoComponent,
+    ChildThreeComponent
   ],
   imports: [
     BrowserModule
